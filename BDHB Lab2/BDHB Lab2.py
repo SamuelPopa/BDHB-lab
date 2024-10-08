197bf0de5ed889a141ef3b6a5c7e3a5217d6ac01
bozdogalex <bozdogalex@gmail.com>
2024-10-08 17:36:13 +0300

    Lab 2 information

---
+++ BDHB Lab2/BDHB Lab2.py
@@ -0,0 +1,13 @@
+#Introduction to Sequence Alignment
+#Types of Sequence Alignment
+#Global Alignment: Needleman-Wunsch Algorithm
+#Local Alignment: Smith-Waterman Algorithm
+#Substitution Matrices: PAM and BLOSUM
+#Applications of Sequence Alignment
+#Lab activities: Practical Sequence Alignment
+    #Using BLAST for Local Alignment
+    #Performing Global and Multiple Sequence Alignment with Clustal Omega
+    #Retrieving and Analyzing Real Genomic Data
+    #Perform assignments 2.1 -> 2.10, compare your results with Blast and Clustal Omega
+#Next week: phylogenetics and phylogenic trees
+
